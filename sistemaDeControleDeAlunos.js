a1c7357e96840c1aa6e7b7e6363815aaff8a775d
SENAIMauricio <maufdezessete@gmail.com>
2022-07-27 16:09:52 -0300

    atividade concluída

---
+++ sistemaDeControleDeAlunos.js
@@ -0,0 +1,12 @@
+let numeroTotal = 13; //número hipotético
+
+for (aluno = 0; aluno <= 13; aluno++) {
+    if (aluno == 0) {
+        console.log("zero: " + aluno)
+    } if (aluno % 2 == 0) {
+        console.log("par: " + aluno)
+    } else if (aluno % 2 != 0) {
+        console.log("ímpar: " + aluno)
+    }
+};
+
